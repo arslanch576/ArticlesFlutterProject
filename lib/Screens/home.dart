f068fcde9f06360c5c893f647b48ba89f10d9f55
Muhammad Arslan Sarwar <arslanch576@gmail.com>
2025-03-11 11:58:17 +0500

    bookmark: state management using setState

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -11,19 +11,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Article> articles=DummyArticleProvider.getDummyArticle();
+  List<Article> articles = DummyArticleProvider.getDummyArticle();
+  List<Article> bookmarks = [];
 
+  void toggleBookmark(Article article){
+    setState(() {
+      if(bookmarks.contains(article)){
+        bookmarks.remove(article);
+      }else{
+        bookmarks.add(article);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Home",
-      home: Scaffold(
+      return Scaffold(
         appBar: AppBar(
           title: Text("Home"),
+          actions: [IconButton(onPressed: () => {}, icon: Icon(Icons.bookmarks,color: bookmarks.isEmpty?Colors.grey:Colors.orangeAccent,))],
         ),
-        body: ListView.builder(itemCount: articles.length, itemBuilder:(context, index) => ArticleWidget(article: articles[index]),),
-      ),
-    );
+        body: ListView.builder(
+          itemCount: articles.length,
+          itemBuilder: (context, index) =>
+              ArticleWidget(article: articles[index],isBookmarked: bookmarks.contains(articles[index]),toggleBookmark: toggleBookmark,),
+        ),
+      );
   }
 }
--- lib/widgets/article_widget.dart
+++ lib/widgets/article_widget.dart
@@ -2,25 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:forms/models/article.dart';
 import 'package:forms/Screens/article_details.dart';
 
-class ArticleWidget extends StatelessWidget{
+class ArticleWidget extends StatefulWidget{
   final Article article;
+  bool isBookmarked;
+  Function(Article) toggleBookmark;
 
-  const ArticleWidget({super.key, required this.article});
+  ArticleWidget({super.key, required this.article, required this.isBookmarked, required this.toggleBookmark});
 
+  @override
+  State<ArticleWidget> createState() => _ArticleWidgetState();
+}
+
+class _ArticleWidgetState extends State<ArticleWidget> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ArticleDetailsPage(article: article),)),
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ArticleDetailsPage(article: widget.article),)),
       child: Row(
         children: [
-          Expanded(child: Image.network(article.image,)),
+          Expanded(child: Image.network(widget.article.image,)),
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(article.title, style: TextStyle(fontSize: 20),),
-                Text("By ${article.author}"),
-                Text(article.date),
+                IconButton(onPressed: () => widget.toggleBookmark(widget.article), icon: Icon(widget.isBookmarked?Icons.bookmark:Icons.bookmark_border),),
+                Text(widget.article.title, style: TextStyle(fontSize: 20),),
+                Text("By ${widget.article.author}"),
+                Text(widget.article.date),
               ],
             ),
           )
@@ -28,5 +36,4 @@ class ArticleWidget extends StatelessWidget{
       ),
     );
   }
-
 }
